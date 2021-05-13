2e0b8fe534b8cfdc9d139cb0e96966cb7d918b2b
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-05-13 09:15:29 -0300

    Add colors (#6)
    * Add colors
    
    * Fix colors

---
+++ src/assets/colors.ts
@@ -0,0 +1,18 @@
+export const Colors = {
+	background: "#202124",
+	white1000: "#ffffff",
+	black1000: "#000000",
+	gray50: "#d9e3e4",
+	gray100: "#9ba9b4",
+	gray200: "#6a6f82",
+	gray300: "#a7a7a7",
+	gray500: "#33363a",
+	gray800: "#222325",
+	discord: "#7289DA",
+	github: "#181717",
+	google: "#4285F4",
+	linkedin: "#0A66C2",
+	stackOverFlow: "#F58025",
+	dribbble: "#EA4C89",
+	behance: "#1769FF",
+};
