cd2decf4472981f3e40adcf8c8e7a76327290747
Alessandro Ramos <70819804+alessandrohenriqueramos@users.noreply.github.com>
2021-05-13 08:47:25 -0300

    Add colors

---
+++ src/assets/colors.ts
@@ -0,0 +1,13 @@
+export const Colors = {
+	primary: "#2a66f7",
+	darkPrimary: "#0f4cde",
+	background: "#202124",
+	white1000: "#ffffff",
+	black1000: "#000000",
+	gray50: "#d9e3e4",
+	gray100: "#9ba9b4",
+	gray200: "#6a6f82",
+	gray300: "#a7a7a7",
+	gray500: "#33363a",
+	gray800: "#222325",
+};
